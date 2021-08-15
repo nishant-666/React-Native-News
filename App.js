69f4ab033ad94276844f3d9e1ffc304903d25161
Nishant Kumar <nishant.k@doodleblue.com>
2021-08-15 22:11:50 +0530

    Part 1

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import All from './screens/All';
+import Business from './screens/Business';
+import HealthScreen from './screens/Health';
+import SportsScreen from './screens/Sports';
+import TechScreen from './screens/Tech';
+import { Icon } from 'react-native-elements'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator tabBarOptions={{showLabel: false, activeTintColor: '#ffc107'}}>
+        <Tab.Screen name="All" component={All}
+          options={{
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <Icon name='rowing' type='feather' name='home' color={props.color}/>
+            ),
+          }} />
+
+        <Tab.Screen name="Business" component={Business}
+          options={{
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <Icon name='rowing' type='feather' name='dollar-sign' color={props.color}/>
+            ),
+          }} />
+
+        <Tab.Screen name="Health" component={HealthScreen}
+          options={{ 
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <Icon name='rowing' type='feather' name='heart' color={props.color}/>
+            ),
+          }} />
+
+        <Tab.Screen name="Sports" component={SportsScreen}
+          options={{ 
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <Icon name='rowing' type='ionicon' name="tennisball-outline" color={props.color}/>
+            ),
+          }} />
+
+        <Tab.Screen name="Tech" component={TechScreen}
+          options={{ 
+            headerShown: false,
+            tabBarIcon: (props) => (
+              <Icon name='rowing' type='ionicon' name="hardware-chip-outline" color={props.color}/>
+            ),
+            }} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Sports.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { Divider, NativeBaseProvider } from 'native-base';
+
+export default function Sports() {
+    return (
+        <NativeBaseProvider>
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.text}>Sports</Text>
+            </View>
+            <View>
+                <View style={styles.flex}>
+                    <Text style={styles.title}>Title</Text>
+                    <Text style={styles.date}>Date</Text>
+                </View>
+                <View style={styles.description}>
+                    <Text style={styles.title}>Description</Text>
+                </View>
+            </View>
+            <Divider my={2} bg="#bdbdbd"/>
+            <View>
+                <View style={styles.flex}>
+                    <Text style={styles.title}>Title</Text>
+                    <Text style={styles.date}>Date</Text>
+                </View>
+                <View style={styles.description}>
+                    <Text style={styles.title}>Description</Text>
+                </View>
+            </View>
+            <Divider my={2} bg="#bdbdbd"/>
+            <View>
+                <View style={styles.flex}>
+                    <Text style={styles.title}>Title</Text>
+                    <Text style={styles.date}>Date</Text>
+                </View>
+                <View style={styles.description}>
+                    <Text style={styles.title}>Description</Text>
+                </View>
+            </View>
+            <Divider my={2} bg="#bdbdbd"/>
+            <View>
+                <View style={styles.flex}>
+                    <Text style={styles.title}>Title</Text>
+                    <Text style={styles.date}>Date</Text>
+                </View>
+                <View style={styles.description}>
+                    <Text style={styles.title}>Description</Text>
+                </View>
+            </View>
+            <Divider my={2} bg="#bdbdbd"/>
+        </View>
+        </NativeBaseProvider>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+        backgroundColor: '#e0e0e0',
+        border: '1px solid #bdbdbd'
+    },
+    text: {
+        fontSize: 24,
+    },
+    flex: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20
+    },
+    title: {
+        fontSize: 20
+    },
+    date: {
+        fontSize: 20
+    },
+    description: {
+        padding: 20
+    }
+});
